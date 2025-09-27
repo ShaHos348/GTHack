3fff33f611f803415a410b589544fdd016b3d75e
kcao0918 <kcao0918@gmail.com>
2025-09-26 21:19:50 -0400

    patient history boiler

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,10 @@
 import { Link } from "react-router-dom";
-import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <div>
       <div className=" text-3xl text-amber-200">
         <Link to="/patienthistory">Go to PatientHistory</Link>
-      </div>
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button>Click me</Button>
       </div>
     </div>
   );
